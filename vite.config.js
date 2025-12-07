74bde2de2a7a2a900f2bb6a7762d6270949ec0c5
Prathamesh Balkrishna Shinde <109452236+Patushinde@users.noreply.github.com>
2025-12-07 16:07:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vastu-consultant/'     // <-- repository name
+  base: '/vastu-consultant'     // <-- repository name
 })
 
